43330e2cc2b2afe2f3deb4bb2f75e85edc5e737b
masudmolla6 <masudmolla2937@gmail.com>
2026-02-27 16:06:24 +0600

    Element Access function

---
+++ Module 2/Vector_Element_Access.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v={34,34,45,67,89,100};
+
+    // cout << v[2] << endl;
+    cout << v.at(2) << endl;
+    cout << v.front() << endl;
+    cout << v.back() << endl;
+    return 0;
+}
