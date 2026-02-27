e8eacaa8848afaa1eb007cdf4ef36cc6e47ab0e9
masudmolla6 <masudmolla2937@gmail.com>
2026-02-27 16:11:29 +0600

    Vector Iterators

---
+++ Module 2/Vector_Iterators.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v={34,56,78,34,67};
+    for (auto it=v.begin(); it<v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    
+    return 0;
+}
