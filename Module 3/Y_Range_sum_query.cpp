3a85e3d5bc5c712820afd6cb2676730df6606ca3
masudmolla6 <masudmolla2937@gmail.com>
2026-03-02 21:16:02 +0600

    Range Sum Query prefix sum

--- Module 3/Y_Range_sum_query.cpp
+++ Module 3/Y_Range_sum_query.cpp
@@ -9,25 +9,35 @@ using namespace std;
 int main() {
     int n, q;
     cin >> n >> q;
-    int a[n+1];
+    long long int a[n+1];
+
     for (int i = 1; i <= n; i++)
     {
         cin >> a[i];
+    }
+
+    long long int pre[n+1];
+
+    pre[1]=a[1];
+
+    for (int i = 2; i <= n; i++)
+    {
+        pre[i]=pre[i-1]+a[i];
     }
 
     while (q--)
     {
         int l,r;
         cin >> l >> r;
-        int sum=0;
-        for (int i = l; i <= r; i++)
-        {
-            sum+=a[i];
+        long long int sum=0;
+        if(l==1){
+            sum=pre[r];
+        }
+        else{
+            sum=pre[r]-pre[l-1];
         }
         cout << sum << endl;
     }
-
-    
     
     return 0;
 }
