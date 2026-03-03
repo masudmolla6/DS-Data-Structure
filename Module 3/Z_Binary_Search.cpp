1acba3301bf46a0a4c65985623008cd583fea3e6
masudmolla6 <masudmolla2937@gmail.com>
2026-03-03 14:07:01 +0600

    Binary Search

--- Module 3/Z_Binary_Search.cpp
+++ Module 3/Z_Binary_Search.cpp
@@ -4,34 +4,54 @@ using namespace std;
 int main() {
     int n, q;
     cin >> n >> q;
-    vector<int> v(n);
+
+    int a[n];
 
     for (int i = 0; i < n; i++)
     {
-        cin >> v[i];
+        cin >> a[i];
     }
+
+    sort(a, a+n);
+
 
     while (q--)
     {
-        int x;
-        cin >> x;
+        
+        int val;
+        cin >> val;
         int flag=0;
 
-        for (int i = 0; i < n; i++)
-        {
-            if(v[i]== x){
-                flag=1;
-            }
-        }
+        int l=0;
+        int r=n-1;
         
+        while (l<=r)
+        {
+            int mid=(l+r)/2;
+
+            if(a[mid]==val){
+                flag=1;
+                break;
+            }
+            else if(a[mid] > val){
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+
+        }
+
         if(flag==1){
-            cout << "found" << endl;
+        cout << "found" << endl;
         }
         else{
             cout << "not found" << endl;
         }
 
     }
+    
+    
     
     return 0;
 }
