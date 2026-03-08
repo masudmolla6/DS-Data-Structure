55030eebb88b73b64161e4995296112e1c6825a0
masudmolla6 <masudmolla2937@gmail.com>
2026-03-08 17:04:16 +0600

    Doubly linked lisdt

---
+++ Module 9/Doubly_linked_list.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+        int val;
+        Node* pre;
+        Node* next;
+
+    Node(int val){
+        this->val=val;
+        this->pre=NULL;
+        this->next=NULL;
+    }
+};
+
+void print_linked_list(Node* temp){
+    if(temp==NULL){
+        return;
+    }
+
+    cout << temp->val << " ";
+    print_linked_list(temp->next);
+
+}
+
+int main() {
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* tail=new Node(30);
+
+    head->next=a;
+    a->pre=head;
+    a->next=tail;
+    tail->pre=a;
+
+    print_linked_list(head);
+    
+    return 0;
+}
