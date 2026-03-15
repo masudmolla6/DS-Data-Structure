d88848509b41519e499f122096a118f906b04ae2
masudmolla6 <masudmolla2937@gmail.com>
2026-03-15 15:35:08 +0600

    list capacity

--- module 10/list_Capacity.cpp
+++ module 10/list_Capacity.cpp
@@ -2,6 +2,29 @@
 using namespace std;
 
 int main() {
+    // list<int>l;
+    // cout << l.size() << endl;
+    // cout << l.max_size() << endl;
+
+    list<int> l={2,3,5,7,8};
     
+    // l.clear();
+    // cout << l.size() << endl;
+
+    // if(l.empty()){
+    //     cout << "Yes List is Empty" << endl;
+    // }
+    // else{
+    //     cout << "No List is Not empty" << endl;
+    // }
+
+    l.resize(9);
+
+    for(int val:l){
+        cout << val << " ";
+    }
+
+    // cout << l.size() << endl;
+
     return 0;
 }
