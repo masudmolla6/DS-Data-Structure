266b44bc575dfe52b99cf6235feb1f8a02c80600
masudmolla6 <masudmolla2937@gmail.com>
2026-03-22 08:27:03 +0600

    List element access and iterators

---
+++ module 10/List_element_access_and_iterators.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    list<int> l={23,45,68,89,23,12,45,67};
+
+    // cout << l.back() << endl;
+    // cout << l.front() << endl;
+
+    // cout << *next(l.begin(), 1) << endl;
+
+    cout << *l.begin() << endl;
+    cout << l.back() << endl;
+    cout << *l.end() << endl;
+
+    // for (auto it=l.begin(); it!=l.end(); it)
+    // {
+    //     /* code */
+    // }
+    
+    return 0;
+}
