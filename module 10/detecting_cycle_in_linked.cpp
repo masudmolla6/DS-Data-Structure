933a9221b8fab61b64bb5973a489ab9f9d45b71c
masudmolla6 <masudmolla2937@gmail.com>
2026-03-23 18:20:13 +0600

    detect sycle in linked list

---
+++ module 10/detecting_cycle_in_linked.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+
+    Node(int val){
+        this->val=val;
+        this->next=NULL;
+    }
+};
+
+int main() {
+    Node* head=new Node(10);
+    Node* a=new Node(20);
+    Node* b=new Node(30);
+    Node* c=new Node(40);
+    Node* d=new Node(50);
+
+    head->next=a;
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    // d->next=b;
+
+    Node* fast=head;
+    Node* slow=head;
+    int flag=0;
+
+    while (fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+
+        if(fast==slow){
+            flag=1;
+            break;
+        }
+    }
+    
+    if(flag==1){
+        cout << "sycle detected" << endl;
+    }
+    else{
+        cout << "Ther are no sycle in this list" << endl;
+    }
+    
+    return 0;
+}
