32dd97c55349a8f28b417a09fa2dbc1eb5ac61f4
masudmolla6 <masudmolla2937@gmail.com>
2026-04-08 12:51:36 +0600

    delete at tail

---
+++ Practice 2/delete_at_tail_doubly.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* pre;
+        Node* next;
+
+    Node(int val){
+        this->val=val;
+        this->pre=NULL;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+    Node* newNode=new Node(val);
+
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+
+    tail->next=newNode;
+    newNode->pre=tail;
+    tail=newNode;
+}
+
+void delete_at_tail(Node* &head, Node* &tail){
+    if(head==NULL){
+        return;
+    }
+
+    if(head->next==NULL){
+        delete head;
+        head=NULL;
+        tail=NULL;
+        return;
+    }
+
+    Node* deletedNode=tail;
+
+    tail=tail->pre;
+    tail->next=NULL;
+    delete deletedNode;
+}
+
+void print_linked_list(Node* temp){
+    while(temp != NULL){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node* head=NULL;
+    Node* tail=NULL;
+    int val;
+    while (cin >> val && val!=-1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    delete_at_tail(head, tail);
+    print_linked_list(head);
+    
+    return 0;
+}
