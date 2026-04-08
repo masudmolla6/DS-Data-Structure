5a3bfc72e7dbadcc974f04e35e0273a8975d7c4d
masudmolla6 <masudmolla2937@gmail.com>
2026-04-08 21:02:57 +0600

    delete at any position

---
+++ Practice 2/delete_at_any_position_doubly.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* pre;
+        Node* next;
+
+    Node(int val){
+        this->val=val;
+        this->pre=NULL;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+    Node* newNode=new Node(val);
+
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+
+    tail->next=newNode;
+    newNode->pre=tail;
+    tail=newNode;
+}
+
+int siz_of_linked_list(Node* temp){
+    int count=0;
+
+    while (temp!=NULL)
+    {
+        count++;
+        temp=temp->next;
+    }
+    
+    return count;
+
+}
+
+void delete_at_head(Node* &head, Node* &tail){
+    if(head==NULL){
+        return;
+    }
+
+    if(head->next==NULL){
+        delete head;
+        head=NULL;
+        tail=NULL;
+        return;
+    }
+
+    Node* deletedNode=head;
+
+    head=head->next;
+    if(head!=NULL){
+        head->pre=NULL;
+    }
+    delete deletedNode;
+}
+
+void delete_at_tail(Node* &head, Node* &tail){
+    if(head==NULL){
+        return;
+    }
+
+    if(head->next==NULL){
+        delete head;
+        head=NULL;
+        tail=NULL;
+        return;
+    }
+
+    Node* deletedNode=tail;
+
+    tail=tail->pre;
+    tail->next=NULL;
+    delete deletedNode;
+}
+
+void delete_at_any_position(Node* &head, Node* &tail, int idx){
+    if(head==NULL){
+        return;
+    }
+    int size=siz_of_linked_list(head);
+
+    if(idx<0 ||size<=idx){
+        cout << "invalid Index" << endl;
+        return;
+    }
+    else if(idx==0){
+        delete_at_head(head, tail);
+    }
+    else if(idx==size-1){
+        delete_at_tail(head, tail);
+    }
+    else{
+        Node* temp=head;
+        for (int i = 1; i < idx; i++)
+        {
+            temp=temp->next;
+        }
+        // cout << temp->val << endl;
+        Node* deletedNode=temp->next;
+        Node* nextNode=temp->next->next;
+
+        temp->next=nextNode;
+
+        if(nextNode != NULL){
+            nextNode->pre = temp;
+        }
+        
+        delete deletedNode;
+    }
+}
+
+void print_linked_list(Node* temp){
+    while(temp != NULL){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node* head=NULL;
+    Node* tail=NULL;
+    int val;
+    while (cin >> val && val!=-1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    delete_at_tail(head, tail);
+    print_linked_list(head);
+    
+    return 0;
+}
