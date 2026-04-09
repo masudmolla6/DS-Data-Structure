c544fbf4c9f9bd10c5f75675ba9ff72981161376
masudmolla6 <masudmolla2937@gmail.com>
2026-04-09 11:25:59 +0600

    reverse doubly linked list

---
+++ Practice 2/reverse_doubly_linked_list.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* pre;
+        Node* next;
+
+    Node(int val){
+        this->val=val;
+        this->pre=NULL;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node* &head, Node* &tail, int val){
+    Node* newNode=new Node(val);
+
+    if(head==NULL){
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+
+    tail->next=newNode;
+    newNode->pre=tail;
+    tail=newNode;
+}
+
+void reverse_linked_list(Node* &head, Node* &tail){
+    for (Node *i = head, *j=tail; i!=j && i->pre!=j; i=i->next, j=j->pre)
+    {
+        swap(i->val, j->val);
+    }
+}
+
+void print_linked_list(Node* temp){
+    while(temp != NULL){
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+}
+
+int main() {
+    Node* head=NULL;
+    Node* tail=NULL;
+    int val;
+    while (cin >> val && val!=-1)
+    {
+        insert_at_tail(head, tail, val);
+    }
+
+    reverse_linked_list(head, tail);
+    print_linked_list(head);
+    
+    return 0;
+}
