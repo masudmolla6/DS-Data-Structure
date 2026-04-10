507a1b6752d996c7e80d72ab3909a5b160485f5c
masudmolla6 <masudmolla2937@gmail.com>
2026-04-10 19:27:21 +0600

    complete Modifier function in stl list

--- Practice 3/Modifiers.cpp
+++ Practice 3/Modifiers.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-    list<int>list1={12,34,56,87};
+    list<int>list1={10, 20, 30, 40, 50, 60};
     list<int>list2;
     // list2=list1;
 
@@ -19,14 +19,30 @@ int main() {
     // list2.insert(next(list2.begin(), 2), 500);
     // list2.insert(next(list2.begin(), 2), list3.begin(), list3.end());
     // cout << *next(list2.begin(), 2) << endl;
-
-    list2.erase(next(list3.begin(), 2));
-
-
-    for (auto it = list2.begin(); it != list2.end(); it++)
-    {
-        cout << *it << " ";
-    }
     
+    
+    // list2.erase(next(list2.begin(), 2));
+    // list2.erase(next(list2.begin(), 1), next(list2.begin(), 4));
+
+    // replace(list2.begin(), list2.end(), 20, 35);
+
+
+    // for (auto it = list2.begin(); it != list2.end(); it++)
+    // {
+    //     cout << *it << " ";
+    // }
+
+    auto it=find(list2.begin(), list2.end(), 300);
+
+    // if(it==list2.end()){
+    //     cout << "Not Found" << endl;
+    // }
+    // else{
+    //     cout << "Found" << endl;
+    // }
+
+    for(int val : list2){
+        cout << val << endl;
+    }
     return 0;
 }
