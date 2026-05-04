97b3710021031b2123356f6e087b230c9c2098ce
masudmolla6 <masudmolla2937@gmail.com>
2026-05-04 09:58:01 +0600

    stl list recap

---
+++ Practice 4/list.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    list<int> l={1,2,3,4,5,6};
+
+    // for (auto it=l.begin(); it!=l.end(); it++)
+    // {
+    //     cout << *it << endl;
+    // }
+
+    for (int val:l)
+    {
+        cout << val << " ";
+    }
+    
+    
+    return 0;
+}
