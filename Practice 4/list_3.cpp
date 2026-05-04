5c3cc6c97128145a2b7006be8b37862cb099aa23
masudmolla6 <masudmolla2937@gmail.com>
2026-05-04 13:22:18 +0600

    find function

--- Practice 4/list_3.cpp
+++ Practice 4/list_3.cpp
@@ -3,14 +3,26 @@ using namespace std;
 
 int main() {
     list<int> list1={2,3,4,5, 34,45,67, 56,34,93};
-    // list1.erase(next(list1.begin(), 3));
+    list1.erase(next(list1.begin(), 3));
 
-    list1.erase(next(list1.begin(), 3), next(list1.begin(), 6));
+    // list1.erase(next(list1.begin(), 3), next(list1.begin(), 6));
 
+    // replace(list1.begin(), list1.end(), 34, 200);
 
-    for (int val:list1)
-    {
-        cout << val << " ";
+    auto it =find(list1.begin(), list1.end(), 34);
+
+    // cout << *it <<endl;
+
+    // for (int val:list1)
+    // {
+    //     cout << val << " ";
+    // }
+
+    if(it==list1.end()){
+        cout << "Not Found" << endl;
+    }
+    else{
+        cout << "Found" << endl;
     }
 
     return 0;
