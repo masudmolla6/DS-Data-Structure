36651ecdcbfc87384d81445f7fe4914242b3de86
masudmolla6 <masudmolla2937@gmail.com>
2026-06-15 16:11:14 +0600

    postOrder Practice

---
+++ Practice 7/postorder.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+Node* input_binary_tree_lavel_order(){
+    int val;
+    cin >> val;
+    Node* root;
+
+    if(val==-1) root=NULL;
+    else root=new Node(val);
+
+    queue<Node*> q;
+
+    if(root) q.push(root);
+
+    while (!q.empty())
+    {
+        // node ber kore ana.
+        Node* p=q.front();
+        q.pop();
+
+        // oi node niye kaj kora.
+        int l,r;
+        cin >> l >> r;
+
+        Node* myLeft=NULL;
+        Node* myRight=NULL;
+
+        if(l!=-1) myLeft=new Node(l);
+        if(r!=-1) myRight=new Node(r);
+
+        p->left=myLeft;
+        p->right=myRight;
+
+
+        // parent mode er left & right queue te push kora.
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+
+    }
+    
+    return root;
+
+}
+
+void postOrder(Node* root){
+    if(!root)return;
+    
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->val << " ";
+}
+
+int main() {
+    Node* root=input_binary_tree_lavel_order();
+
+    postOrder(root);
+
+    return 0;
+}
