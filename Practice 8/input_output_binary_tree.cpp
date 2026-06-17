6ae7896090759e70e6cbf79cee9abb8b4fdc97e4
masudmolla6 <masudmolla2937@gmail.com>
2026-06-17 20:30:01 +0600

    practice binary tree input and output

---
+++ Practice 8/input_output_binary_tree.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+Node* input_binary_tree(){
+    int val;
+    cin >> val;
+    Node* root;
+
+    if (val==-1) root==NULL;
+    else root=new Node(val);
+
+    queue<Node*> q;
+
+    if(root) q.push(root);
+
+    while (!q.empty())
+    {
+        // q thake first node ber kore niye asa.
+        Node* p=q.front();
+        q.pop();
+
+        // p-> Parent node er left & right children create kora.
+        int l,r;
+        cin >> l >> r;
+
+        Node* myLeft=NULL;
+        Node* myRight=NULL;
+
+        if(l!=-1) myLeft=new Node(l);
+        if(r!=-1) myRight=new Node(r);
+
+        p->left=myLeft;
+        p->right=myRight;
+
+        // parent er left & right ke q te push kora.
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+
+    }
+
+    return root;
+    
+}
+
+void print_binary_tree(Node* root){
+    queue<Node*> q;
+
+    if(root) q.push(root);
+
+    while (!q.empty())
+    {
+        // q thake first node ber kore niye asa.
+        Node* p=q.front();
+        q.pop();
+
+        // oi node niye kaj kora 
+        cout << p->val << " ";
+
+        // parent er left & right ke q te push kora.
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+
+    }
+    
+}
+
+
+
+int main() {
+    Node* root=input_binary_tree();
+    print_binary_tree(root);
+    return 0;
+}
