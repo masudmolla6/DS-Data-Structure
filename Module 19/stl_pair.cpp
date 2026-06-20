107ea58252f1d4040c4a621f0359a4078b8d7ee0
masudmolla6 <masudmolla2937@gmail.com>
2026-06-20 19:09:48 +0600

    pair of vector

--- Module 19/stl_pair.cpp
+++ Module 19/stl_pair.cpp
@@ -2,8 +2,28 @@
 using namespace std;
 
 int main() {
-    pair<string, int> p;
-    p=make_pair("Masud",3);
-    cout << p.first <<" "<<p.second << endl;
+
+    // stl pair
+    // pair<string, int> p;
+    // p=make_pair("Masud",3);
+    // p={"Masud", 34};
+    // cout << p.first <<" "<<p.second << endl;
+
+    // pair of vector
+    int n;
+    cin >> n;
+    vector<pair<int, int>> v(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first <<" "<< v[i].second << endl;
+    }
+    
+
     return 0;
 }
