ac332c87531b4b160a528cd277cb393bda0450e4
masudmolla6 <masudmolla2937@gmail.com>
2026-06-21 09:53:45 +0600

    solved level order

---
+++ Module 19/Node_Level.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+Node* input_binary_tree(){
+    int val;
+    cin >> val;
+
+    Node* root;
+    if(val==-1) root=NULL;
+    else root=new Node(val);
+
+    queue<Node*> q;
+
+    if(root) q.push(root);
+
+    while (!q.empty())
+    {
+        // queue thake front ta ber kore niye asa.
+        Node* p=q.front();
+        q.pop();
+
+        // oi node niye kaj kora.
+        int l,r;
+        cin >> l >> r;
+
+        Node* myLeft=NULL;
+        Node* myRight=NULL;
+
+        if(l!=-1) myLeft=new Node(l);
+        if(r!=-1) myRight=new Node(r);
+
+        p->left=myLeft;
+        p->right=myRight;
+
+        // parent Node er left & right queue te push kora.
+        if(p->left) q.push(p->left);
+        if(p->right) q.push(p->right);
+
+    }
+    
+    return root;
+
+}
+
+int node_level(Node* root, int x){
+    queue<pair<Node*,int>>q;
+    if(root) q.push({root, 1});
+
+    while (!q.empty())
+    {
+        pair<Node*,int> parent=q.front();
+        q.pop();
+
+        Node* node =parent.first;
+        int level=parent.second;
+        if(node->val==x) return level;
+
+        if(node->left) q.push({node->left, level+1});
+        if(node->right) q.push({node->right, level+1});
+
+    }
+    return 0;
+}
+
+
+
+int main() {
+    Node* root=input_binary_tree();
+    int x;
+    cin >> x;
+    int level=node_level(root,x);
+    cout << level << " ";
+}
