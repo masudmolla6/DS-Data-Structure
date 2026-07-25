1703680d22c0004337530693778030f2fc90ab65
masudmolla6 <masudmolla2937@gmail.com>
2026-07-25 19:26:10 +0600

    preorder traversal

---
+++ Practice 9/inorder_traversal.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+
+    Node(int val){
+        this->val=val;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+Node* input_binary_tree(){
+    int val;
+    cin >> val;
+    Node* root;
+    if(val==-1)
+        root=NULL;
+    else
+        root=new Node(val);
+
+    queue<Node*> q;
+
+    if(root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        // queue thake node ber kore ana.
+        Node* p=q.front();
+        q.pop();
+
+        // oi node niye kaj kora.
+        int l,r;
+        cin >> l >> r;
+
+        Node* MyLeft=NULL;
+        Node* myRight=NULL;
+
+        if(l!=-1)
+            MyLeft=new Node(l);
+        
+        if(r!=-1)
+            myRight=new Node(r);
+
+        if(MyLeft)
+            p->left=MyLeft;
+        
+        if(myRight)
+            p->right=myRight;
+        
+        // Parent node er left and right queue te push kora;
+        if(p->left)
+            q.push(p->left);
+        
+        if(p->right)
+            q.push(p->right);
+    }
+    
+    return root;
+}
+
+void print_inorder_traversal(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+    cout << root->val << " ";
+
+    print_inorder_traversal(root->left);
+    print_inorder_traversal(root->right);
+}
+
+int main() {
+    Node* root=input_binary_tree();
+    print_inorder_traversal(root); 
+    return 0;
+}
