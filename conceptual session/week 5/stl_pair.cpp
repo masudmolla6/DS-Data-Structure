c5257e6ad2c0823fbee945c4b08e90372b6fdb31
masudmolla6 <masudmolla2937@gmail.com>
2026-08-06 20:04:01 +0600

    recap stl pair

---
+++ conceptual session/week 5/stl_pair.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // Normal stl pair
+    pair<string, int> p;
+    // p=make_pair("Masud Rana", 34);
+    p={"Hello", 10};
+    // cout << p.first << " " <<  p.second << endl;
+
+    // Pair Of Vector 
+    vector<
+    return 0;
+}
