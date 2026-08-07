d431ee7f5fc7388eba4a9e304553ffa3128db872
masudmolla6 <masudmolla2937@gmail.com>
2026-08-07 10:08:43 +0600

    node lavel

--- conceptual session/week 5/stl_pair.cpp
+++ conceptual session/week 5/stl_pair.cpp
@@ -9,6 +9,21 @@ int main() {
     // cout << p.first << " " <<  p.second << endl;
 
     // Pair Of Vector 
-    vector<
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> v(n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+        
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].first << " " << v[i].second << endl;
+    }
+    
+
     return 0;
 }
